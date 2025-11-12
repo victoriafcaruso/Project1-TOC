47449d7cec9bde3c4ef9b6c84a251a1adf7dd5e3
Victoria Caruso <vcaruso@student10.cse.nd.edu>
2025-11-12 18:12:44 -0500

    comments in our file

--- src/team_sat.py
+++ src/team_sat.py
@@ -1,4 +1,16 @@
 
-print("HI")
+
+# Function that reads the NEW team_test inputs
+
+
 if __name__ == "__main__":
+    # read inputs
+    # call on parser
+    # call on out solver
+
+    # call on function that reads outputs two one csv file PER method (2)
+
+    # Make plots for backtrack and brute force with those csv files
+
+
     print("Hello")
